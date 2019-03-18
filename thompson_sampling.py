12d7726789d2844fa81aae076392568db5d2fdf1
twheelertech <tony@wheelertechconsulting.com>
2019-03-18 01:02:59 -0500

    Setting up thompson algo

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -22,11 +22,29 @@ for i in range(N):
 
 
 # Implementing a Random Strategy and Thompson Sampling
-
-
+strategies_selected_rs = []
+strategies_selected_ts = []
+total_reward_rs = 0
+total_reward_ts = 0
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
+for n in range(0, N):
     # Random Strategy
+    strategy_rs = random.randrange(d)
+    strategies_selected_rs.append(strategies_rs)
+    reward_rs = X[n, strategy_rs]
+    total_reward_rs = total_reward_rs + reward_rs
 
     # Thompson Sampling
+    strategy_ts = 0
+    max_random = 0
+    for i in range(0, d):
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
+            strategy_ts = i
+    
+            
 
 
 # Computing the Absolute and Relative Return
